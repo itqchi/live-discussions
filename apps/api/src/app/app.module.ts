3c8905e1a29bfa4baca659e132f8019b31e8b8e3
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-07 23:12:44 +0200

    Wire database module into API app module

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { RoomMembershipService } from '../rooms/room-membership.service';
 import { RoomsController } from '../rooms/rooms.controller';
 import { RoomsService } from '../rooms/rooms.service';
 
 @Module({
+  imports: [DatabaseModule],
   controllers: [RoomsController],
   providers: [RoomsService, RoomMembershipService],
 })
