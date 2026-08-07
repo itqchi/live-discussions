a5b18452ad5a74a70c1988409407b963aa9b83ca
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-07 23:17:56 +0200

    Enable global API validation

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -1,9 +1,19 @@
 import 'reflect-metadata';
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app/app.module';
 
 async function bootstrap(): Promise<void> {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      transform: true,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+    }),
+  );
+
   app.enableCors({ origin: true, credentials: true });
   await app.listen(Number(process.env.PORT ?? 3000));
 }
