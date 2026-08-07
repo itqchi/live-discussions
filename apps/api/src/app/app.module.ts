90fd5cbf17b0891e1841d55c719e63ccc925d275
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-07 23:25:33 +0200

    Load local environment variables in Nest API

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,7 +1,14 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { RoomsModule } from '../rooms/rooms.module';
 
 @Module({
-  imports: [RoomsModule],
+  imports: [
+    ConfigModule.forRoot({
+      isGlobal: true,
+      cache: true,
+    }),
+    RoomsModule,
+  ],
 })
 export class AppModule {}
