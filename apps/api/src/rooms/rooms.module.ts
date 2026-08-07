aa4d093ca0a42168d4a6cf3fc4a01cb31a1db3ab
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-07 23:17:29 +0200

    Encapsulate rooms feature in Nest module

---
+++ apps/api/src/rooms/rooms.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
+import { RoomMembershipService } from './room-membership.service';
+import { RoomsController } from './rooms.controller';
+import { RoomsService } from './rooms.service';
+
+@Module({
+  imports: [DatabaseModule],
+  controllers: [RoomsController],
+  providers: [RoomsService, RoomMembershipService],
+})
+export class RoomsModule {}
