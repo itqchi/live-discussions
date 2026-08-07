4d60eaa07121774832dce36f05c7fb4c291c32f1
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-07 22:05:00 +0200

    Register room membership service

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RoomMembershipService } from '../rooms/room-membership.service';
 import { RoomsController } from '../rooms/rooms.controller';
 import { RoomsService } from '../rooms/rooms.service';
 
 @Module({
   controllers: [RoomsController],
-  providers: [RoomsService],
+  providers: [RoomsService, RoomMembershipService],
 })
 export class AppModule {}
