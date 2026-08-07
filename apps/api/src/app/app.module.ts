38417928db6b2cb16f358d568ed1542457c29f16
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-07 23:17:41 +0200

    Keep AppModule focused on feature modules

--- apps/api/src/app/app.module.ts
+++ apps/api/src/app/app.module.ts
@@ -1,12 +1,7 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
-import { RoomMembershipService } from '../rooms/room-membership.service';
-import { RoomsController } from '../rooms/rooms.controller';
-import { RoomsService } from '../rooms/rooms.service';
+import { RoomsModule } from '../rooms/rooms.module';
 
 @Module({
-  imports: [DatabaseModule],
-  controllers: [RoomsController],
-  providers: [RoomsService, RoomMembershipService],
+  imports: [RoomsModule],
 })
 export class AppModule {}
