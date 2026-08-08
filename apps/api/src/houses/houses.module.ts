d7e3d45c9b64238b4889771e39f2ed5012a07f60
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-08 13:50:59 +0200

    feat: connect houses to room domain

--- apps/api/src/houses/houses.module.ts
+++ apps/api/src/houses/houses.module.ts
@@ -1,8 +1,10 @@
 import { Module } from '@nestjs/common';
+import { RoomsModule } from '../rooms/rooms.module';
 import { HousesController } from './houses.controller';
 import { HousesService } from './houses.service';
 
 @Module({
+  imports: [RoomsModule],
   controllers: [HousesController],
   providers: [HousesService],
 })
