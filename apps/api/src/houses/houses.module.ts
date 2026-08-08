bd9ab1450ad968edb8675bf7fbd96832abc8ee7b
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-08 13:55:50 +0200

    feat: enable database-backed houses

--- apps/api/src/houses/houses.module.ts
+++ apps/api/src/houses/houses.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
+import { DatabaseModule } from '../database/database.module';
 import { RoomsModule } from '../rooms/rooms.module';
 import { HousesController } from './houses.controller';
 import { HousesService } from './houses.service';
 
 @Module({
-  imports: [RoomsModule],
+  imports: [DatabaseModule, RoomsModule],
   controllers: [HousesController],
   providers: [HousesService],
 })
