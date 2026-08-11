ec16a0c58f2b7b7453b531945320517a5940af58
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-11 12:22:58 +0200

    refactor: rely on global database module

--- apps/api/src/rooms/rooms.module.ts
+++ apps/api/src/rooms/rooms.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { DatabaseModule } from '../database/database.module';
 import { RoomMembershipService } from './room-membership.service';
 import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 
 @Module({
-  imports: [DatabaseModule],
   controllers: [RoomsController],
   providers: [RoomsService, RoomMembershipService],
   exports: [RoomsService, RoomMembershipService],
