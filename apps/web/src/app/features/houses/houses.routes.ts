ff20b655df946017627a379b507044b14ee37476
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-11 18:22:41 +0200

    Provide room API to House routes

--- apps/web/src/app/features/houses/houses.routes.ts
+++ apps/web/src/app/features/houses/houses.routes.ts
@@ -1,11 +1,12 @@
 import type { Routes } from '@angular/router';
+import { RoomApiService } from '../rooms/data-access/room-api.service';
 import { HouseApiService } from './data-access/house-api.service';
 import { HouseFacade } from './data-access/house.facade';
 
 export const HOUSES_ROUTES: Routes = [
   {
     path: ':houseId',
-    providers: [HouseApiService, HouseFacade],
+    providers: [HouseApiService, RoomApiService, HouseFacade],
     loadComponent: () =>
       import('./pages/house-page/house-page.component').then(
         (module) => module.HousePageComponent,
