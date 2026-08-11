7db958a738f5ecd3798fdf0a0df08ed04bbf146a
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-11 17:46:03 +0200

    Register room settings service

--- apps/api/src/rooms/rooms.module.ts
+++ apps/api/src/rooms/rooms.module.ts
@@ -1,12 +1,13 @@
 import { Module } from '@nestjs/common';
 import { RoomCommentsService } from './room-comments.service';
 import { RoomMembershipService } from './room-membership.service';
+import { RoomSettingsService } from './room-settings.service';
 import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 
 @Module({
   controllers: [RoomsController],
-  providers: [RoomsService, RoomMembershipService, RoomCommentsService],
-  exports: [RoomsService, RoomMembershipService, RoomCommentsService],
+  providers: [RoomsService, RoomMembershipService, RoomCommentsService, RoomSettingsService],
+  exports: [RoomsService, RoomMembershipService, RoomCommentsService, RoomSettingsService],
 })
 export class RoomsModule {}
