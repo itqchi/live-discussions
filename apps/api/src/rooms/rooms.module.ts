372eda96bd743076c82a3a01325d1884a014547c
itqchi <32709581+itqchi@users.noreply.github.com>
2026-08-11 14:28:29 +0200

    feat(api): register room comments persistence

--- apps/api/src/rooms/rooms.module.ts
+++ apps/api/src/rooms/rooms.module.ts
@@ -1,11 +1,12 @@
 import { Module } from '@nestjs/common';
+import { RoomCommentsService } from './room-comments.service';
 import { RoomMembershipService } from './room-membership.service';
 import { RoomsController } from './rooms.controller';
 import { RoomsService } from './rooms.service';
 
 @Module({
   controllers: [RoomsController],
-  providers: [RoomsService, RoomMembershipService],
-  exports: [RoomsService, RoomMembershipService],
+  providers: [RoomsService, RoomMembershipService, RoomCommentsService],
+  exports: [RoomsService, RoomMembershipService, RoomCommentsService],
 })
 export class RoomsModule {}
